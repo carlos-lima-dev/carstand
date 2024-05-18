c38b45b98b2852c5cc3d1915edaf5f0b3c0c66d0
carloslima1991 <carloslima.ptit@gmail.com>
2024-05-18 22:59:22 +0100

    remove logo image

--- carseller/src/components/Logo/Logo.jsx
+++ carseller/src/components/Logo/Logo.jsx
@@ -5,7 +5,6 @@ const Logo = () => {
   return (
     <div className={styles.container_logo}>
       <Link to="/">
-        <img src="\assets\32px-Eo_circle_red_white_letter-c.svg.png" alt="" />
         <span>CAR</span>
         <span>SELLER</span>
       </Link>
