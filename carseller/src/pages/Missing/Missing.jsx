7566f597cd4fd56d8f67883b2bab7cb81fdc4033
carloslima1991 <carloslima.ptit@gmail.com>
2024-05-18 22:54:31 +0100

    missing page update

--- carseller/src/pages/Missing/Missing.jsx
+++ carseller/src/pages/Missing/Missing.jsx
@@ -1,4 +1,5 @@
 import {motion} from "framer-motion";
+import {Link} from "react-router-dom";
 
 const Missing = () => {
   return (
@@ -6,10 +7,24 @@ const Missing = () => {
       initial={{opacity: 0, x: "100%"}}
       animate={{opacity: 1, x: 0}}
       exit={{opacity: 0, x: "-100%"}}
-      transition={{duration: 1, ease: "easeInOut"}} // Tempo da transição em segundos
-    >
+      transition={{duration: 1, ease: "easeInOut"}}>
       {" "}
-      <div>Page not Found 404</div>
+      <div
+        style={{
+          display: "flex",
+
+          justifyContent: "center",
+          minHeight: "100vh",
+          alignItems: "center",
+          backgroundColor: "black",
+          color: "#fff",
+          letterSpacing: "3px",
+        }}>
+        <p style={{color: "#fff"}}>Page not Found 404 / </p>
+        <Link to="/">
+          <p style={{color: "red"}}>Go Back Home</p>
+        </Link>
+      </div>
     </motion.div>
   );
 };
